3bcc327bef1943fac1d8edf8daaf54d5aefe5dc2
Jonathan Thom and Marty Kovach <marty@>
2016-11-15 15:29:57 -0800

    Will translate up to a twelve digit number

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -56,32 +56,60 @@ class Fixnum
     thousands_array = numbers.pop(3)
     thousands_number = thousands_array.join("").to_i
     millions_array = numbers.pop(3)
+    millions_number = millions_array.join("").to_i
     billions_array = numbers.pop(3)
-
-    last_two = hundreds_array.last(2)
+    billions_number = billions_array.join("").to_i
+    last_two_hundreds = hundreds_array.last(2)
+    last_two_thousands = thousands_array.last(2)
+    last_two_millions = millions_array.last(2)
+    last_two_billions = billions_array.last(2)
     final_words = []
     hundred = ""
     thousand = ""
-    if hundreds_array.size.eql?(1)
-      hundreds = individuals.fetch(self)
-    elsif hundreds_array.size.eql?(2) && self.<(20)
-      hundreds = individuals.fetch(self)
-    elsif hundreds_array.size.eql?(2)
-      tens_word = deca.fetch(hundreds_array[0].to_i)
-      singles_word = individuals.fetch(hundreds_array[1].to_i)
+    million = ""
+    billion = ""
+    if billions_array.size.eql?(1)
+      billions = individuals.fetch(billions_number) + " billion"
+    elsif billions_array.size.eql?(2) && billions_number.<(20)
+      billions = individuals.fetch(billions_number) + " billion"
+    elsif billions_array.size.eql?(2)
+      tens_word = deca.fetch(billions_array[0].to_i)
+      singles_word = individuals.fetch(billions_array[1].to_i)
       words = tens_word.concat(" " + singles_word)
-      hundreds = words.strip
-    elsif hundreds_array.size.eql?(3) && last_two.join("").to_i.<(20)
-      hundreds_word = hundreds.fetch(hundreds_array[0].to_i)
-      teens_word = individuals.fetch(last_two.join("").to_i)
+      billions = words.strip + " billion"
+    elsif billions_array.size.eql?(3) && last_two_billions.join("").to_i.<(20)
+      hundreds_word = hundreds.fetch(billions_array[0].to_i)
+      teens_word = individuals.fetch(last_two_billions.join("").to_i)
       words = hundreds_word.concat(" " + teens_word)
-      hundreds = words.strip
-    elsif hundreds_array.size.eql?(3)
-      singles_word = individuals.fetch(hundreds_array[2].to_i)
-      tens_word = deca.fetch(hundreds_array[1].to_i)
-      hundreds_word = hundreds.fetch(hundreds_array[0].to_i)
+      billions = words.strip + " billion"
+    elsif billions_array.size.eql?(3)
+      singles_word = individuals.fetch(billions_array[2].to_i)
+      tens_word = deca.fetch(billions_array[1].to_i)
+      hundreds_word = hundreds.fetch(billions_array[0].to_i)
       words = hundreds_word.concat(" " +tens_word+ " "+singles_word)
-      hundreds = words.strip
+      billions = words.strip + " billion"
+    end
+
+    if millions_array.size.eql?(1)
+      millions = individuals.fetch(millions_number) + " million"
+    elsif millions_array.size.eql?(2) && millions_number.<(20)
+      millions = individuals.fetch(millions_number) + " million"
+    elsif millions_array.size.eql?(2)
+      tens_word = deca.fetch(millions_array[0].to_i)
+      singles_word = individuals.fetch(millions_array[1].to_i)
+      words = tens_word.concat(" " + singles_word)
+      millions = words.strip + " million"
+    elsif millions_array.size.eql?(3) && last_two_millions.join("").to_i.<(20)
+      hundreds_word = hundreds.fetch(millions_array[0].to_i)
+      teens_word = individuals.fetch(last_two_millions.join("").to_i)
+      words = hundreds_word.concat(" " + teens_word)
+      millions = words.strip + " million"
+    elsif millions_array.size.eql?(3)
+      singles_word = individuals.fetch(millions_array[2].to_i)
+      tens_word = deca.fetch(millions_array[1].to_i)
+      hundreds_word = hundreds.fetch(millions_array[0].to_i)
+      words = hundreds_word.concat(" " +tens_word+ " "+singles_word)
+      millions = words.strip + " million"
     end
 
     if thousands_array.size.eql?(1)
@@ -93,9 +121,9 @@ class Fixnum
       singles_word = individuals.fetch(thousands_array[1].to_i)
       words = tens_word.concat(" " + singles_word)
       thousands = words.strip + " thousand"
-    elsif thousands_array.size.eql?(3) && last_two.join("").to_i.<(20)
+    elsif thousands_array.size.eql?(3) && last_two_thousands.join("").to_i.<(20)
       hundreds_word = hundreds.fetch(thousands_array[0].to_i)
-      teens_word = individuals.fetch(last_two.join("").to_i)
+      teens_word = individuals.fetch(last_two_thousands.join("").to_i)
       words = hundreds_word.concat(" " + teens_word)
       thousands = words.strip + " thousand"
     elsif thousands_array.size.eql?(3)
@@ -106,6 +134,30 @@ class Fixnum
       thousands = words.strip + " thousand"
     end
 
+    if hundreds_array.size.eql?(1)
+      hundreds = individuals.fetch(self)
+    elsif hundreds_array.size.eql?(2) && self.<(20)
+      hundreds = individuals.fetch(self)
+    elsif hundreds_array.size.eql?(2)
+      tens_word = deca.fetch(hundreds_array[0].to_i)
+      singles_word = individuals.fetch(hundreds_array[1].to_i)
+      words = tens_word.concat(" " + singles_word)
+      hundreds = words.strip
+    elsif hundreds_array.size.eql?(3) && last_two_hundreds.join("").to_i.<(20)
+      hundreds_word = hundreds.fetch(hundreds_array[0].to_i)
+      teens_word = individuals.fetch(last_two_hundreds.join("").to_i)
+      words = hundreds_word.concat(" " + teens_word)
+      hundreds = words.strip
+    elsif hundreds_array.size.eql?(3)
+      singles_word = individuals.fetch(hundreds_array[2].to_i)
+      tens_word = deca.fetch(hundreds_array[1].to_i)
+      hundreds_word = hundreds.fetch(hundreds_array[0].to_i)
+      words = hundreds_word.concat(" " +tens_word+ " "+singles_word)
+      hundreds = words.strip
+    end
+
+    final_words.push(billions)
+    final_words.push(millions)
     final_words.push(thousands)
     final_words.push(hundreds)
     final_words.join(" ").strip
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -21,4 +21,37 @@ describe('Fixnum#numbers_to_words') do
   it("Will translate a four digit number to a word") do
     expect(2145.numbers_to_words()).to(eq("two thousand one hundred forty five"))
   end
+
+  it("Will translate a five digit number to a word") do
+    expect(32145.numbers_to_words()).to(eq("thirty two thousand one hundred forty five"))
+  end
+
+  it("Will translate a six digit number to a word") do
+    expect(932145.numbers_to_words()).to(eq("nine hundred thirty two thousand one hundred forty five"))
+  end
+
+  it("will translate a seven digit number into words") do
+    expect(4234424.numbers_to_words()).to(eq('four million two hundred thirty four thousand four hundred twenty four'))
+  end
+
+  it("Will translate a eight digit number into words") do
+    expect(54032504.numbers_to_words()).to(eq('fifty four million thirty two thousand five hundred four'))
+  end
+
+  it("Will translate a nine digit number into words") do
+    expect(532953842.numbers_to_words()).to(eq('five hundred thirty two million nine hundred fifty three thousand eight hundred forty two'))
+  end
+
+  it("Will translate a ten digit number into words") do
+    expect(3532953842.numbers_to_words()).to(eq('three billion five hundred thirty two million nine hundred fifty three thousand eight hundred forty two'))
+  end
+
+  it("Will translate an eleven digit number into words") do
+    expect(93532953842.numbers_to_words()).to(eq('ninety three billion five hundred thirty two million nine hundred fifty three thousand eight hundred forty two'))
+  end
+
+  it("Will translate an twelve digit number into words") do
+    expect(293532953842.numbers_to_words()).to(eq('two hundred ninety three billion five hundred thirty two million nine hundred fifty three thousand eight hundred forty two'))
+  end
+
 end
